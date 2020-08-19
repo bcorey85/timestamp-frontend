d098744f1ead9844a8c78708c65a41097943b30f
Brandon Corey <bcorey85@gmail.com>
2020-08-19 13:54:16 -0500

    fix failing drawer test

--- components/AppLayout/Drawer/__tests__/Drawer.test.tsx
+++ components/AppLayout/Drawer/__tests__/Drawer.test.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-
 import { shallow } from 'enzyme';
+
+import {
+	mockStore,
+	MockReduxProvider
+} from '../../../../test/__mocks__/mockRedux';
 
 import { Drawer } from '../Drawer';
 
 describe('Drawer', () => {
 	it('renders', () => {
-		const wrapper = shallow(<Drawer />);
+		const wrapper = shallow(
+			<MockReduxProvider reduxStore={mockStore}>
+				<Drawer />
+			</MockReduxProvider>
+		);
 		expect(wrapper).not.toBeNull();
 	});
 });
