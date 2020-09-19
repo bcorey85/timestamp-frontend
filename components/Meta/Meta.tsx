9fb0386b5dd16598363b0d33c1f1f213d33ce015
Brandon Corey <bcorey85@gmail.com>
2020-09-18 23:54:41 -0500

    add meta viewport tag

--- components/Meta/Meta.tsx
+++ components/Meta/Meta.tsx
@@ -11,6 +11,10 @@ const Meta = ({ pageTitle }: Props): JSX.Element => {
 		<Head>
 			<title>Timestamp App | {pageTitle}</title>
 			<link rel='icon' href='/favicon.ico' />
+			<meta
+				name='viewport'
+				content='width=device-width, initial-scale=1.0'
+			/>
 		</Head>
 	);
 };
