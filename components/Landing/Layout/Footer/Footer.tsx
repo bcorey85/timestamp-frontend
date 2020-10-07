11f6b87b272882652921fa1de6abf5e68a774e94
Brandon Corey <bcorey85@gmail.com>
2020-10-07 18:25:36 -0500

    update footer information

--- components/Landing/Layout/Footer/Footer.tsx
+++ components/Landing/Layout/Footer/Footer.tsx
@@ -5,14 +5,17 @@ import styles from './Footer.module.scss';
 const Footer = (): JSX.Element => {
 	return (
 		<footer className={styles.footer}>
-			<p>Copyright &copy; {new Date().getFullYear()}</p>
-			<p>Built & Designed in KCMO</p>
 			<a href='https://www.bcdev.co'>
 				<img
 					src='/images/bc-logo.svg'
 					alt='Brandon Corey Web Development logo'
 				/>
 			</a>
+			<p>Copyright &copy; {new Date().getFullYear()}</p>
+			<a href='https://www.bcdev.co'>
+				<p>Brandon Corey Web Development</p>
+			</a>
+			<p>Built & Designed in KCMO</p>
 		</footer>
 	);
 };
